6bc14e6d0a223122cb626871e372530d917757a9
韦东锏 <weidongjian@gmail.com>
2021-08-28 11:23:04 +0800

    整合考勤结果

--- main.py
+++ main.py
@@ -19,17 +19,24 @@ COLUMN_TIME = 8
 
 
 class workItem:
-    def __init__(self, name, idValue, startTime, endTime):
-        self.name = name
-        self.idValue = idValue
-        self.startTime = startTime
-        self.endTime = endTime
+    def __init__(self):
+        self.startTime = ""
+        self.endTime = ""
 
     def reset(self):
-        self.name = ""
-        self.endTime = ""
         self.startTime = ""
         self.idValue = ""
+
+
+class UserItem:
+    def __init__(self, name, idValue):
+        self.name = name
+        self.idValue = idValue
+        self.dict = {}  # 打卡信息，key是日期，value是打卡list
+
+
+# 总的加班信息,key是员工账号， value是UserItem
+workMap = {}
 
 
 def isWeekend(dataStr):
@@ -37,51 +44,79 @@ def isWeekend(dataStr):
     return chinese_calendar.is_holiday(date)
 
 
-def calOverTimeReal(item):
+def calOverTimeReal(startTime, endTime):
     try:
-        timeOne = datetime.datetime.strptime(item.startTime, '%H:%M')
-        timeTwo = datetime.datetime.strptime(item.endTime, '%H:%M')
+        timeOne = datetime.datetime.strptime(startTime, '%H:%M')
+        timeTwo = datetime.datetime.strptime(endTime, '%H:%M')
     except ValueError:
-        return
+        return 0
 
     timeDiffSec = (timeTwo - timeOne).total_seconds()
     if timeOne.hour < 12:
         # 减去午休的时间
         timeDiffSec = timeDiffSec - LAUNCH_BREAK_SECOND
 
-    finalTime = datetime.timedelta(seconds=timeDiffSec)
-    workResult = str(finalTime)
-    mat = "{:10}\t{:10}\t{:10}"
-    print(mat.format(item.name, item.idValue, workResult))
+    return timeDiffSec
 
 
-def updateItem(tempItem, nameValue, idValue, timeValue):
-    tempId = tempItem.idValue
-    if len(tempId) == 0:
-        tempItem.idValue = idValue
-
-    sameID = tempItem.idValue == idValue
-    if sameID:
-        # 代表是同个用户的加班
-        tempItem.name = nameValue
-        if len(tempItem.startTime) == 0:
-            tempItem.startTime = timeValue
-        else:
-            tempItem.endTime = timeValue
+def getUserItemFromMap(idValue, nameValue):
+    if idValue in workMap.keys():
+        return workMap[idValue]
     else:
-        # 不同用户，需要计算加班时间
-        calOverTimeReal(tempItem)
+        valueItem = UserItem(nameValue, idValue)
+        workMap[idValue] = valueItem
+        return valueItem
 
-    return sameID
+
+def getWorkListFromUser(user_item, dateValue):
+    if dateValue in user_item.dict.keys():
+        return user_item.dict[dateValue]
+    else:
+        workList = []
+        user_item.dict[dateValue] = workList
+        return workList
+
+
+def updateWorkData(user_item, dateValue, timeValue):
+    workList = getWorkListFromUser(user_item, dateValue)
+    workList.append(timeValue)
+
+
+def calTotalWorkTime(workDic):
+    totalSecond = 0
+    keyList = []
+    for key in workDic.keys():
+        keyList.append(key)
+        workList = workDic[key]
+        if len(workList) >= 2:
+            startTime = workList[0]
+            endTime = workList[-1]
+            time_real = calOverTimeReal(startTime, endTime)
+            totalSecond = totalSecond + time_real
+
+    totalMin = totalSecond // 60
+    totalHour = totalMin // 60
+    remainMin = totalMin % 60
+    workResult = str(int(totalHour)) + ":" + str(int(remainMin))
+    mat = "{:6}\t{:10}"
+    mat_format = mat.format("总加班时长", workResult)
+    print(mat_format + " 加班日期 " + str(keyList))
+
+
+def calculateResult():
+    for value in workMap.values():
+        mat = "{:10}\t{:12}"
+        print(mat.format(value.name, value.idValue), end="")
+        workDic = value.dict
+        calTotalWorkTime(workDic)
 
 
 def main():
-    # sourceFile = input("请输入考勤文件地址：")
-    sourceFile = "/Users/weigan/Downloads/7月考勤全员.xlsx"
+    sourceFile = input("请输入考勤文件地址：")
+    # sourceFile = "/Users/weigan/Downloads/7月考勤全员.xlsx"
     workbook = openpyxl.load_workbook(sourceFile.strip(), read_only=True)
     sheet = workbook.worksheets[1]
     sheet.iter_rows(min_row=5)
-    tempItem = workItem("", "", "", "")
     for row in sheet.iter_rows(min_row=5, values_only=True):
         dateValue = row[COLUMN_DATE]
         weekValue = row[COLUMN_WEEK]
@@ -90,10 +125,10 @@ def main():
         timeValue = row[COLUMN_TIME]
         if not isWeekend(dateValue):
             continue
-        result = updateItem(tempItem, nameValue, idValue, timeValue)
-        if not result:
-            tempItem.reset()
-            tempItem.startTime = timeValue
+        user_item = getUserItemFromMap(idValue, nameValue)
+        updateWorkData(user_item, dateValue, timeValue)
+
+    calculateResult()
 
 
 if __name__ == '__main__':
